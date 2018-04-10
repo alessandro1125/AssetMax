eeca1c8d2ba6800c48b90ed92daf3306ee7dca2b
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-04-10 20:25:52 +0200

    2025

--- src/main/java/utils/SqlUtils.java
+++ src/main/java/utils/SqlUtils.java
@@ -58,6 +58,12 @@ public class SqlUtils {
 
         String query = "INSERT INTO " + table + " (" + keys + ")" +
                 " VALUES (" + values + ");";
+        byte[] queryBytes = query.getBytes();
+        for (Byte bytes : queryBytes){
+            if (bytes == 0)
+                System.out.println("PORCO DIO C'è UN CHAR CHE VALE 0");
+        }
+
         return executeUpdate(connection, query);
     }
 
@@ -211,7 +217,7 @@ public class SqlUtils {
      * @param query String
      * @return boolean
      */
-    public static boolean executeUpdate(Connection connection, String query){
+    private static boolean executeUpdate(Connection connection, String query){
         Statement stmt;
         try {
             stmt = connection.createStatement();
@@ -250,5 +256,44 @@ public class SqlUtils {
         }
 
         return null;
+    }
+
+    /**
+     *
+     * @return Connection
+     */
+    private static Connection getConnection(){
+
+        Connection connection = null;
+        try {
+
+            try {
+
+                Class.forName("org.postgresql.Driver");
+
+            } catch (ClassNotFoundException e) {
+
+                System.out.println("Where is your PostgreSQL JDBC Driver? "
+                        + "Include in your library path!");
+                e.printStackTrace();
+                return null;
+
+            }
+
+            String url = "jdbc:postgresql://ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/" +
+                    "d2qht4msggj59q?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory&" +
+                    "sslmode=require&user=sagdjsuxgvztxk&" +
+                    "password=8be153a38455d94b7422704cec7de29ab6b0772c07f40a94f71932387641710a";
+
+            connection = DriverManager.getConnection(url);
+
+        }
+        catch (Exception e) {
+            System.err.println("Database connection failed");
+            System.err.println(e.getMessage());
+        }
+
+        return connection;
+
     }
 }
