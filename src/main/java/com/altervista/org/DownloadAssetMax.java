3f6d20d76a0cd570b135dfdf0a72569141e11c38
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-04-16 23:35:33 +0200

    2335

--- src/main/java/com/altervista/org/DownloadAssetMax.java
+++ src/main/java/com/altervista/org/DownloadAssetMax.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @WebServlet(
         name = "DownloadZip",
@@ -18,8 +20,18 @@ public class DownloadAssetMax extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response){
         try {
+            OutputStream outputStream = response.getOutputStream();
             FileInputStream fileInputStream = new FileInputStream(getClass().getResource("/AssetMax.zip").getFile());
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = fileInputStream.read(buffer)) > 0){
+                outputStream.write(buffer, 0, length);
+            }
+            fileInputStream.close();
+            outputStream.flush();
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
