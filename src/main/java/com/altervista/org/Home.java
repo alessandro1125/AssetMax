948cd0c75be8df488b0d38e93bf7522c49792db3
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-04-17 19:54:51 +0200

    1954

--- src/main/java/com/altervista/org/Home.java
+++ src/main/java/com/altervista/org/Home.java
@@ -18,7 +18,7 @@ public class Home extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
         try {
             RequestDispatcher view;
-            view = req.getRequestDispatcher("https://assetmax.herokuapp.com/login");
+            view = req.getRequestDispatcher("/login");
             view.forward(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
